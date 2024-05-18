bd60100d37c624238a83d8c1b372259602d70102
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-05-18 13:09:17 +0200

    Fuindamentals Part 1 and 2

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,176 @@
+/* Activating strict mode */
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio'; // error
+// const private = 534; // error
+
+/* Functions */
+// function logger() {
+//   console.log("My name is Sibongiseni Mxinwa");
+// }
+
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+// const num = Number('23');
+
+/* Function Declarations vs. Expressions */
+// Function declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+/* Function  Expressions */
+// const calcAge2 = function calcAge2(birthYear) {
+//   const age2 = 2037 - birthYear;
+//   return age2;
+// };
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+/* Arrow Functions */
+// const calcAge2 = function calcAge2(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Sibongiseni'));
+// console.log(yearsUntilRetirement(1995, 'Sibo'));
+
+/* Functions calling other functions */
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+/* Reviewing Functions */
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 60 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired 🎉`);
+//   }
+// };
+
+// console.log(yearsUntilRetirement(1991, "Sibongiseni"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
+
+/* Coding Challenge #1 */
+
+/* 
+CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+
+
+Your tasks:
+
+Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+
+Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+
+Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+
+Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
+
+Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
+
+
+
+TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+
+TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
+ */
+
+/* Coding Challenge Solution #1 */
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+// // Test 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log("No team wins...");
+//   }
+// };
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// checkWinner(576, 111);
+
+// // Test 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
+/* Introduction to Arrays */
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
